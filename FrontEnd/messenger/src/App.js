f0d2f0e299e78a1a71cc059671450c04edb4fa8e
Bern <nexeland@gmail.com>
2023-09-12 14:04:09 +0600

    del

--- FrontEnd/messenger/src/App.js
+++ FrontEnd/messenger/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import HomePage from './main/pages/HomePage';
-import Messenger from './messenger/Messenger';
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import MessengerPage from './pages/MessengerPage'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/messenger/*" element={<Messenger/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/mess" element={<MessengerPage/>} />
       </Routes>
     </>
   );
